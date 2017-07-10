564610b29c281dd73417b0f6f196df93025fbd98
Sang <https://github.com/sangchunahn>
2017-07-10 12:15:48 -0600

    added react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
